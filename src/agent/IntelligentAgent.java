d87743e0691090742a2d42ff925e07409b858382
Daniel H da Silva <danielhensilva@gmail.com>
2015-07-16 13:24:16 -0300

    Change IntelligentAgent.java to become BDI compliance.

--- src/agent/IntelligentAgent.java
+++ src/agent/IntelligentAgent.java
@@ -7,4 +7,52 @@ import planner.*;
 import utils.*;
 
 public abstract class IntelligentAgent {
+
+    /*
+    BDI-interpreter
+    ---------------
+    initialize-state();
+    repeat
+        options := option-generator(event-queue);
+        selected-options := deliberate(options);
+        update-intentions(selected-options);
+        execute();
+        get-new-external-events();
+        drop-successful-attitudes();
+        drop-impossible-attitudes();
+    end repeat
+    */
+
+    public IntelligentAgent() {
+
+    }
+
+    public void inializeState() {
+
+    }
+
+    public List<Option> generateOptions(Percepts percept) {
+
+    }
+
+    public List<SelectedOption> deliberate(List<Option> options) {
+
+    }
+
+    public void Execute() {
+
+    }
+
+    public List<Percepts> getNewExternalPercepts() {
+
+    }
+
+    public void DropSucessfulAttitudes() {
+
+    }
+
+    public void DropImpossibleAttitudes() {
+
+    }
+    
 }
